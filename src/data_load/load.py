c99141e965463d67e8a6f4251214231bcbe8ea52
@Andrian03 <andrian.tabach@gmail.com>
2026-04-30 09:47:06 +0300

    Implement data_load: CSV to SQLite DB

--- src/data_load/load.py
+++ src/data_load/load.py
@@ -1,9 +1,51 @@
+import pandas as pd
+import sqlite3
 import os
 
-def load_data():
-    print("--- Module: Data Load ---")
-    os.makedirs('data/raw', exist_ok=True)
-    print("Папка data/raw готова для завантаження файлів.")
+print("--- Module: Data Load ---")
 
-if __name__ == "__main__":
-    load_data()
+
+csv_path = "data/raw/passengers.csv"
+
+
+db_path = "data/database.db"
+
+
+if not os.path.exists(csv_path):
+    print("CSV файл не знайдено!")
+    exit(1)
+
+
+df = pd.read_csv(csv_path)
+print(f"CSV завантажено. Кількість рядків: {len(df)}")
+
+
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS passengers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    year INTEGER,
+    month INTEGER,
+    branch TEXT,
+    passengers INTEGER
+)
+""")
+
+print("Таблиця створена або вже існує")
+
+
+cursor.execute("DELETE FROM passengers")
+
+
+df.to_sql("passengers", conn, if_exists="append", index=False)
+
+print("Дані успішно записані в БД")
+
+
+conn.commit()
+conn.close()
+
+print("Data Load завершено")
